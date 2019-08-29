f83730d91871fdb9a8fb982817f6bc77ce51064b
Cphe <cpheboss@hotmail.com>
2019-08-29 18:04:10 +0200

    Add model and admin site

--- ludomanagerapp/admin.py
+++ ludomanagerapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from ludomanagerapp.models import Game, Member, GameSession, GameInstance, Borrowing
+# register your models here.
+admin.site.register(Game)
+admin.site.register(Member)
+admin.site.register(GameSession)
+admin.site.register(GameInstance)
+admin.site.register(Borrowing)
--- ludomanagerapp/models.py
+++ ludomanagerapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    minimum_players = models.IntegerField()
+    maximum_players = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    add_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+class GameSession(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+
+class GameInstance(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.PROTECT)
+    players = models.ManyToManyField(Member, null=True, blank=True)
+    game_session = models.ForeignKey(GameSession, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.game.name + '(' + str(self.players.count()) + ')'
+
+class Borrowing(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.PROTECT)
+    member = models.ForeignKey(Member, on_delete=models.PROTECT)
+    date_out = models.DateField()
+    expected_date_in = models.DateField()
+    date_in = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.game) + ' by ' + str(self.member)
